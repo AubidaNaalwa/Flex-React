6fac4b2be2f83feae705c8f628d2d0d6be7e6e4a
DrowRanger <54094000+AubidaNaalwa@users.noreply.github.com>
2021-01-30 15:00:01 +0200

    adding files

---
+++ lesson/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { BrowserRouter as Router, Route, Link, BrowserRouter} from 'react-router-dom'
+
+class Home extends Component{
+
+    render(){
+
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Home;
---
+++ lesson/src/components/LogIn.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import { BrowserRouter as Router, Route, Link, BrowserRouter} from 'react-router-dom'
+
+class LogIn extends Component{
+
+    render(){
+
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default LogIn;
